18a1f9d972f6606dfc30e73f37c34a2c7cc240be
liuhuyu <735077491@qq.com>
2013-12-16 21:14:24 +0800

    name consistency

--- src/com/crowdtut/common/Config.java
+++ src/com/crowdtut/common/Config.java
@@ -12,16 +12,16 @@ import org.bee.tl.ext.jfinal.BeetlRenderFactory;
 import com.crowdtut.controller.IndexController;
 
 public class Config extends JFinalConfig {
-    public void configConstant(Constants me) {
-        me.setDevMode(true);
-        me.setMainRenderFactory(new BeetlRenderFactory());
+    public void configConstant(Constants constants) {
+        constants.setDevMode(true);
+        constants.setMainRenderFactory(new BeetlRenderFactory());
     }
 
-    public void configRoute(Routes me) {
-        me.add("/", IndexController.class);
+    public void configRoute(Routes routes) {
+        routes.add("/", IndexController.class);
     }
 
-    public void configPlugin(Plugins me) { }
-    public void configInterceptor(Interceptors me) { }
-    public void configHandler(Handlers me) { }
+    public void configPlugin(Plugins plugins) { }
+    public void configInterceptor(Interceptors interceptors) { }
+    public void configHandler(Handlers handlers) { }
 }
